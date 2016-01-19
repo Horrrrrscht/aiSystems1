f2b6dbe1ed676e324105c7225a488ba12e1334ad
Horrrrrscht <b.jacobsen93@gmail.com>
2016-01-19 13:05:52 +0100

    Update Node.h
    variablen update

--- NodesEdges/Node.h
+++ NodesEdges/Node.h
@@ -19,8 +19,8 @@ typedef vector<Neuron> Layer;
 
 struct Connection
 {
-	double weight;
-	double deltaWeight;
+	double strength;
+	double deltaStrength;
 };
 
 class Node
@@ -40,8 +40,8 @@ public:
 	void addOutgoingEdges(Edge* rEdge) { m_outgoingEdges.push_back(rEdge); }
 	void addIncomingEdges(Edge* rEdge) { m_incomingEdges.push_back(rEdge); }
 
-	void eraseOutgoingEdge(Edge* rEdge);
-	void eraseIncomingEdge(Edge* rEdge);
+	// void eraseOutgoingEdge(Edge* rEdge);
+	// void eraseIncomingEdge(Edge* rEdge);
 
 
 private:
@@ -61,15 +61,15 @@ private:
 class Neuron
 {
 public:
-	Neuron(unsigned numOutputs, unsigned myIndex);
-	void setOutputVal(double val) { m_outputVal = val;  }
-	double getOutputVal(void) const { return m_outputVal; }
+	Neuron(unsigned numOutputs, unsigned myId);
+	void setOutputVal(double val) { m_ergebnis = val;  }
+	double getOutputVal(void) const { return m_ergebnis; }
 	void feedForward(const Layer &prevLayer);
 	void calcOutputGradients(double targetVal);
 	void calcHiddenGradients(const Layer &nextLayer);
 	void updateInputWeights(Layer &prevLayer);
-	vector<Connection> &getOutputWeights() { return m_outputWeights; }
-	vector<Connection> m_outputWeights;
+	vector<Connection> &getOutputWeights() { return m_outputStrengths; }
+	vector<Connection> m_outputStrengths;
 	~Neuron();
 
 private:
@@ -87,49 +87,5 @@ private:
 
 
 
-//class InNode : public NeuralNode
-//{
-//public:
-//	InNode();
-//	~InNode();
-//
-//private:
-//
-//
-//};
-
-
-
-//class HiddenNode : public NeuralNode
-//{
-//public:
-//	HiddenNode();
-//	~HiddenNode();
-//
-//private:
-//
-//};
-
-
-
-//class OutNode : public NeuralNode
-//{
-//public:
-//	OutNode();
-//	~OutNode();
-//
-//private:
-//
-//};
-
-//class NeuralNode : public Node
-//{
-//public:
-//	NeuralNode();
-//	~NeuralNode();
-//
-//private:
-//	double m_value;
-//};
 
 #endif
