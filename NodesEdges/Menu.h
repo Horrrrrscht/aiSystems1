21e522329dcd13ad04e75f54e7dccb93f20a65c1
Bennet <b.jacobsen93@gmail.com>
2016-01-20 00:30:47 +0100

    Die Anfänge der konsoleneingabe... not sophisticated

---
+++ NodesEdges/Menu.h
@@ -0,0 +1,33 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <vector>
+
+using namespace std;
+
+class menu
+{
+public:
+	menu();
+	vector<double> getinputdata() {};
+	~menu();
+
+private:
+
+	vector<double> m_inputdata
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // !MENU_H
